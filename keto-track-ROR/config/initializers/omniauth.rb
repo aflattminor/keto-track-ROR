f53af600232a5404765990e9c1957ec033a0ad42
aflattminor <ajflattery50@gmail.com>
2017-10-10 15:40:25 -0400

    omni-auth

---
+++ keto-track-ROR/config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "830182461671-027dle1ht6l8etiad00s7sk7pauai881.apps.googleusercontent.com", "D968bb_dJx1hAkJWWkrz5t_Y"
+end
